760221f60e9d3a8320a49d124dbac779b57be3db
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-02-18 18:51:23 +0000

    Terminate when you type exit

--- simpleshell/2shell.c
+++ simpleshell/2shell.c
@@ -8,27 +8,28 @@
 int main()
 {
 	char *str = NULL;
-	size_t n; 
+	size_t n;
 	char *stkn;
 	pid_t mypid;
 	int status, i;
 
-	while(1)
+	while (1)
 	{
 	printf("$ ");
-	getline(&str, &n, stdin); 
+	getline(&str, &n, stdin);
 
 	stkn = strtok(str, " \n");/*solves the issue of executing command*/
 
 	char **arr = malloc(sizeof(char *) * 32);
+
 	arr[0] = stkn;
-	
+
 	if (strcmp(arr[0], "exit") == 0)
 		exit(0);
 
 	i = 1;
 
-	while(stkn != NULL)
+	while (stkn != NULL)
 	{
 		stkn = strtok(NULL, " \n");
 		arr[i] = stkn;
@@ -41,7 +42,7 @@ int main()
 		perror("Error");
 		return (1);
 	}
-	else if(mypid == 0)/*solves the issue of exiting*/
+	else if (mypid == 0)/*solves the issue of exiting*/
 	{
 	execve(arr[0], arr, NULL);
 	}
@@ -50,7 +51,7 @@ int main()
 		wait(NULL);/*suspends the parent from action until the child finishes*/
 	}
 	}
-	
+
 	free(str);
 	return (0);
 }
