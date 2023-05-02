f87ae0b3f7b9168e61875f30f5442e7a79c3b5ba
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-05-02 14:45:58 +0000

    Add lists exercise files

---
+++ lately/Lists/1test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+list = [1,2,3,4,20,2,3,3]
+print(list)#!/usr/bin/python
+print(type(list))
+# A list can store different data types
+list2 = [1,"nice", 2.0, 10,2]
+print(type(list2[1]))
+print(type(list2[2]))
