49d18db0029c6085792b778db3ba85e81964a523
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-05-17 21:05:49 +0000

    Add sizeof.c

---
+++ revision/sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main: Entry point
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	printf("The size of int is %ld\n", sizeof(int));
+	printf("The size of char is %ld\n", sizeof(char));
+	printf("The size of float is %ld\n", sizeof(float));
+}
