430f48ed62d98b6873a4427a90874be539b3a1c2
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-05-21 08:10:26 +0000

    Add typedef files

---
+++ revision/1-typedef.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef int number;
+int main(void)
+{
+	number age = 20;
+
+	printf("I am %d years old\n", age);
+	
+	return (0);
+}
---
+++ revision/2-typedef.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	number age = 20;
+
+	printf("I am %d years old\n", age);
+	
+	return (0);
+}
