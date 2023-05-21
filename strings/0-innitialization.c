4326d830ffa870e76be76ffaae65ea68665bceb3
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-05-21 07:58:01 +0000

    Add 0-innitialization.c

---
+++ strings/0-innitialization.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char name[] = "Caleb";
+	char * town = "Isebania";
+	
+	printf("My name is %s\n", name);
+	printf("I grew up in %s\n", town);
+
+	return (0);
+}
+
