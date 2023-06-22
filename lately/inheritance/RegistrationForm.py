f5bffb8477597c1e5c34382aaa4c4ca3f8aeead9
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-06-22 12:27:36 -0700

    Add RegistrationForm.py

--- lately/inheritance/RegistrationForm.py
+++ lately/inheritance/RegistrationForm.py
@@ -1,2 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # RegistraionForm.py
+
+
+class Form:
+    def __init__(self, name, email, password, confirm_password):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.confirm_password = confirm_password
+
+    def validate(self):
+        if self.name and self.email and self.password and self.confirm_password:
+            if self.password != self.confirm_password:
+                print("The passwords do not match")
+            if "@" not in self.email or ".com" not in self.email:
+                print("Enter a valid email")
+        else:
+            print("Please fill in the empty fields")     
+
+
+class Agent(Form):
+    def __init__(self, name, email, password, confirm_password, auuid):
+        self.auuid = auuid
+        super().__init__(name, email, password, confirm_password)
+
+
+class Sstaff(Form):
+    def __init__(self, name, email, password, confirm_password, department):
+        self.department = department
+        super().__init__(name, email, password, confirm_password)
+
+Agent1 = Agent("Irene", "irene@gmaicom", "dadada", "dadada", 1732)
+Agent1.validate()
