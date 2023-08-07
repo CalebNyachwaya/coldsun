6893f6cd5d7f7b40d135ae47f7aa7e3b182fcb87
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-07 06:23:51 -0700

    Update 4-ArraysHassnode.c

--- arrays/4-ArraysHassnode.c
+++ arrays/4-ArraysHassnode.c
@@ -6,41 +6,37 @@
  * Return: Always 0
  */
 
-int main()
+int main(void)
+
 {
-	int a[ ] = {2, 4, 20, 0, 10};
-	
-	printf("First element: %d", a[0]); /* First element: 2 */
+	int a[] = {2, 4, 20, 0, 10};
+
+	printf("First element: %d\n", a[0]); /* First element: 2 */
 
 	printf("Base Address of the Array:\n");
-	/* both printf statements below will yield the same result */
-	printf("%p\n", &a[0]);
-	printf("%p\n", a);
-	
-	
+	printf("Address: %p\n", &a[0]);
+	printf("Address: %p\n", a);
+
 	printf("Address of Elements:\n");
-	printf("%p\n", &a[1]);
-	printf("%p\n\n", a+1);
+	printf("Address of a[1]: %p\n", &a[1]);
+	printf("Address of a[1]: %p\n\n", a + 1);
 
-	printf("%p\n", &a[2]);
-	printf("%p\n\n", a+2);
-
-	printf("%p\n", &a[3]);
-	printf("%p\n\n", a+3);
+	printf("Address of a[2]: %p\n", &a[2]);
+	printf("Address of a[2]: %p\n\n", a + 2);
 
 
 	printf("Value of the first Element:\n");
-	printf("%d\n", *a);
-	printf("%d\n", a[0]);
+	printf("Value: %d\n", *a);
+	printf("Value: %d\n", a[0]);
 
 
 	printf("Element values:\n");
-	printf("%d\n", a[1]);
-	printf("%d\n\n", *(a + 1));
+	printf("Value: %d\n", a[1]);
+	printf("Value: %d\n\n", *(a + 1));
 
-	printf("%d\n", a[2]);
-	printf("%d\n\n", *(a + 2));
+	printf("Value: %d\n", a[2]);
+	printf("Value: %d\n\n", *(a + 2));
 
-	printf("%d\n", a[4]);
-	printf("%d\n", *(a + 4));
+	printf("Value: %d\n", a[4]);
+	printf("Value: %d\n", *(a + 4));
 }
